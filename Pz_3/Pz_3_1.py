5e9653eb40830462e2f3fdd1ff6ef32da5ecafce
dencraz <daniil2007332@gmail.com>
2024-10-24 18:42:56 +0300

    практические

--- Pz_3/Pz_3_1.py
+++ Pz_3/Pz_3_1.py
@@ -1,1 +1,12 @@
-print("hihgg")
+while True:
+    try:
+        a = int(input("Введите трехзначное число: "))
+        if 100 <= a <= 999:
+            print("Вы ввели трехзначное число.")
+            reversed_number = str(a % 10) + str(a % 100 // 10) + str(a // 100)
+            print("Перевернутое число =", reversed_number)
+            break
+        else:
+            print("Число не трехзначное. Пожалуйста, попробуйте снова.")
+    except ValueError:
+        print("Ошибка: Пожалуйста, введите корректное целое число.")
